e2589d712c47b53a6ac0f7c167cc799c35795ba5
rkhatana67 <89466804+rkhatana67@users.noreply.github.com>
2021-09-21 22:23:18 -0400

    Update index.js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -8,13 +8,13 @@ data (){
 
 created() {
         fetch('https://randomuser.me/api')
-        .then( response => response.json())
+        .then(response => response.json())
         .then((responseJson) => {
             console.log(responseJson);
             this.person = responseJson.results[0]
         })
         .catch( err => {
-            console.error(err)
+            console.error((err));
         })
   }
   }
